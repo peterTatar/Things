50d374a5d25799bf6bd78f5b8404756a1e716afd
Peter Tatar <mail.peter.tatar@gmail.com>
2018-07-03 11:54:45 +0200

    feltöltes

---
+++ ASPNETMVC/Controllers/FirstPageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETMVC.Controllers
+{
+    public class FirstPageController : Controller
+    {
+        // GET: FirstPage
+        public ActionResult Index() {
+          ViewBag.ThisCanBeEverything = "Hello Peti " + DateTime.Now.ToString();
+          ViewBag.ThisCanBeEverything1 = "Hello Peti1 " + DateTime.Now.ToString();
+          ViewBag.ThisCanBeEverything2 = "Hello Peti2 " + DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
---
+++ ASPNETMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETMVC.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult About()
+    {
+      using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=r:\Temp\Peti\ASPNETMVC\ASPNETMVC\App_Data\Database1.mdf;Integrated Security=True")) {
+        connection.Open();
+
+        SqlCommand command = connection.CreateCommand();
+        command.CommandText = "SELECT Message FROM AboutMessages WHERE Id = 2";
+
+        var reader = command.ExecuteReader();
+        reader.Read();
+        string databaseMessage = reader.GetString(0);
+      
+        ViewBag.Message = "Your application description page. " + databaseMessage;
+      }
+      
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
